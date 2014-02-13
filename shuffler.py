7e74458b6ce75600197bd26f1156e01059780e40
leoleez <ohhumbleleo@gmail.com>
2014-02-13 11:58:36 -0800

    Step 2
    I added what I think will account for human error, I will not place it
    into my preexisting code yet, but I think it’ll work. In the mean time
    I will work on building a comparison tool, which is more useful to the
    objective of this program.

--- shuffler.py
+++ shuffler.py
@@ -4,8 +4,9 @@
 #Since all output is inherently ordered in this case, I thought this would be a fun little practice session.
 
 # Goals:
-# 1. Create a shuffler that can shuffle any amount of stacks.
-# 2. Create a program that determines the original list and the new list and figure out what
+# 1. Create a shuffler that can shuffle any amount of stacks. **Complete**
+# 1.a. Human Error, account for human error that can influence the outcome.
+# 2. Create a program that determines the original list and the new list and figure out what.
 # 3. Create a program that can store this information and put it in a .js data visualization tool.
 
 #Variables. An empty list and test lists.
@@ -46,33 +47,65 @@ def shuffleList2 ( listToShuffle, repeat ):
 ######################################
 ######################################
 
-#Shuffle any amount of cards
+#Shuffle any amount of cards Finished 2-10-14
 def shuffleN ( listToShuffle, stackCount ):
 	iteration = 1
 	stack = []
 
+	#Generates Stacks to the about in stackCount.
 	while iteration <= stackCount:
 		stack.append( [] )
 		iteration += 1
 
+	#Pops from listToShuffle and populates the subStacks.
 	while listToShuffle:
 		for subStack in stack:
 			if listToShuffle:
 				subStack.append( listToShuffle.pop() )
 
+	#Concatenates from the subStacks back to listToShuffle to return the shuffled list.
 	for subStack in stack:
 		listToShuffle += subStack
 
 	return listToShuffle
 
+#Takes the inputs of how many times the shuffler repeats the process and such.
 def shuffler( listToShuffle, repeat, stackCount ):
 	timesShuffled = 0
 	while timesShuffled <= repeat:
-		#This is the code used to make the stacks shuffle.
+		#shuffleN is the code used to make the stacks shuffle.
 		shuffleN( listToShuffle, stackCount )
 		timesShuffled += 1
 
 	return listToShuffle
+
+
+########################################
+########################################
+#Human Touch: This part of the code is to add human error and to add randomization in how stacks are shuffled. This should try to use the code from above to give it a human element.
+#Taking ideas from exchange shuffle, the best way to approach this would be to 'pretend' to add another card to a stack that we just placed a card in. In human randomness terms I believe this would be the best formulation.
+#Obviously this formulation is arbitrary as human error is not as clean as this suggests, but I hope that this would represent abstractly the chance of an error occuring in the shuffle of a deck.
+
+def shuffleError(stackCount):
+	errorChance = random.randint(0, stackCount + 1)
+
+	if stackCount == errorChance:
+		stackCount -= 1
+
+	return stackCount
+
+
+
+
+########################################
+########################################
+#Compares lists.
+#I am designing this so that you would have to assign listToShuffle where the 
+
+def compareChange():
+	pass
+	#Counts Differences.
+	#Gives a list of the elements that are different than the original list.
 
 
 ########################################
@@ -85,6 +118,8 @@ print shuffler( capList, 9, 3 )
 
 
 #print shuffleN( capList, 2)
+
+
 
 
 
